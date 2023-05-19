244fd6218dbc8e4d9842fe57480e7ff4f5c0fb63
huenguyensw <hue.blue@gmail.com>
2023-05-19 10:28:53 +0200

    cm

--- src/components/Playing.js
+++ src/components/Playing.js
@@ -47,10 +47,10 @@ const Playing = () => {
               <div className='player-container' >
                 <h2>{keys[0]}</h2>
                 {results[keys[0]][results[keys[0]].length - 1] === '0'
-                  ? <img src='../rock.png' />
+                  ? <img src='../rock.png' alt='rockimage' />
                   : results[keys[0]][results[keys[0]].length - 1] === '1'
-                    ? <img src='../paper.png' />
-                    : <img src='../scissor.png' />}
+                    ? <img src='../paper.png' alt='paperimage' />
+                    : <img src='../scissor.png' alt='scissorimage' />}
               </div>
               <motion.h3
                 initial={{
@@ -75,10 +75,10 @@ const Playing = () => {
               <div className='player-container' >
                 <h2>{keys[1]}</h2>
                 {results[keys[1]][results[keys[1]].length - 1] === '0'
-                  ? <img src='../rock.png' />
+                  ? <img src='../rock.png' alt='rockimage' />
                   : results[keys[1]][results[keys[1]].length - 1] === '1'
-                    ? <img src='../paper.png' />
-                    : <img src='../scissor.png' />}
+                    ? <img src='../paper.png' alt='paperimage' />
+                    : <img src='../scissor.png' alt='scissorimage' />}
               </div>
             </div>)
           : (
@@ -86,10 +86,10 @@ const Playing = () => {
               <div className='player-container' >
                 <h2>{keys[1]}</h2>
                 {results[keys[1]][results[keys[1]].length - 1] === '0'
-                  ? <img src='../rock.png' />
+                  ? <img src='../rock.png' alt='rockimage' />
                   : results[keys[1]][results[keys[1]].length - 1] === '1'
-                    ? <img src='../paper.png' />
-                    : <img src='../scissor.png' />}
+                    ? <img src='../paper.png' alt='paperimage' />
+                    : <img src='../scissor.png' alt='scissorimage' />}
               </div>
               <motion.h3
                 initial={{
@@ -114,10 +114,10 @@ const Playing = () => {
               <div className='player-container'>
                 <h2>{keys[0]}</h2>
                 {results[keys[0]][results[keys[0]].length - 1] === '0'
-                  ? <img src='../rock.png' />
+                  ? <img src='../rock.png' alt='rockimage' />
                   : results[keys[0]][results[keys[0]].length - 1] === '1'
-                    ? <img src='../paper.png' />
-                    : <img src='../scissor.png' />}
+                    ? <img src='../paper.png' alt='paperimage' />
+                    : <img src='../scissor.png' alt='scissorimage' />}
               </div>
             </div>)
         : gameMode === '1'
